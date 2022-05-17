870b2436f5224a05b1ab5fe41edb0a8e7f679623
Yanggi Song <didrl741@gmail.com>
2022-05-17 21:32:25 +0900

    엔티티 클래스 개발
    # 1차 작업: 회원 엔티티 분석표를 보고 자바 코드를 짠다.
    
    // 예시: Member 클래스
    
    @Entity
    @Getter @Setter
    public class Member {
    
        @Id @GeneratedValue
        private Long id;
        private String name;
        private Address addrss;
        private List<Order> orders = new ArrayList<Order>();
    }
    
    # 2차 작업: 회원 테이블 분석표를 보고 db와 관련해서 연관관계를 설정한다.
    일대 다 관계인 Member와 Order를 분석해보자.
    
    @Entity
    @Getter @Setter
    public class Member {
    
        @Id @GeneratedValue
        @Column(name = "member_id")  //테이블 명
        private Long id;
    
        private String name;
    
        @Embedded    //내장 타입인 Address 포함.
        private Address addrss;
    
    // order table에 있는 member 필드(FK)에 의해서 맵핑된 거울일 뿐이다.
    // Member에서도 orders를 통해 Order를 손댈 수 있고 Order에서도 Member 손댈수 있다면
    // JPA가 헷갈려한다(?). 따라서 연관관계의 주인을 '다'쪽인 Order로 한다.
    // Member쪽이 주인이 아니기 때문에 그냥 거울일 뿐이라는 표시를 해준다.
    
        @OneToMany(mappedBy = "member_id")
        private List<Order> orders = new ArrayList<Order>();
    }
    
    @Entity
    @Table(name = "orders")  // Order클래스가 db에서 관례로 Order가 돼버리기 때문에 orders라고 명시했다 라고 이해.
    @Getter @Setter
    public class Order {
    
        @Id @GeneratedValue
        @Column(name = "order_id")
        private Long id;
    
        @ManyToOne
        @JoinColumn(name = "member_id")  // FK
        private Member member;
    
        @OneToMany(mappedBy = "order")  // 맵핑된 거울. db를 손댈 수 없다(?)
        private List<OrderItem> orderItems = new ArrayList<>();
    
        @OneToOne    // Order와 Delivery는 일대일 관계이다. FK를 둘중 아무데나 둬도 되지만,
         // 엑세스를 더 많이하는 Order에 두는것이 더 낫다.
        @JoinColumn(name = "delivery_id")
        private Delivery delivery;
    
        private LocalDateTime orderDate;    // 주문 시간. java 8에서 제공.
    
        // @Enumerated(EnumType.STRING) : 디폴트는 ORDINAL: 숫자로 1, 2, 3,,
        // 중간에 다른 상태가 없어지거나 생기면 큰일 남. STRING으로 쓰자.
        @Enumerated(EnumType.STRING)
        private OrderStatus status;     // 주문 상태 [ORDER, CANCEL]
    }
    
    # 상속과 DB
    
    자바 코드에서는 item과 Movie 등을 상속분리 시켜놨지만 DB에서는 같은 Item으로 처리할 것이다.
    따라서 Item에는 @DiscriminatorColumn(name="dtype") 를 붙여주고
    Movie에는 @DiscriminatorColumn("M") 을 예시로 붙여놨다.

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address addrss;
+
+    @OneToMany(mappedBy = "member_id")      // order table에 있는 member 필드에 의해서 맵핑된 거울일 뿐이다
+    private List<Order> orders = new ArrayList<Order>();
+}
