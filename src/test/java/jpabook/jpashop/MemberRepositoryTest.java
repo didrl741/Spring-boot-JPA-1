b1e46138b522d33eb4763d7188cdb9d3fe0cafde
Yanggi Song <didrl741@gmail.com>
2022-05-23 22:13:11 +0900

    MemberService
    # @Transactional
    javax와 springframework 2가지중 후자를 추천.
    db를 단순히 읽을 때는 (readOnly = true)가 더 최적화.
    따라서 여기서는 join만 db에 쓰기때문에 클래스단위에는 (readOnly = true) 해주고
    join 함수에만 따로 @Transactional 다시 써주면 최적화.
    
    # 롬복과 생성자 주입
    
    @AllArgsConstructor:
    모든 필드의 생성자 만들어준다.
    
    @RequiredArgsConstructor:
    final 있는 필드만 생성자 만들어준다. 이게 제일 좋다. 처음에 DI가 필요한 필수 필드들은 이걸로 주입하고
    나머지 자잘한 필드들은 나중에 따로 넣어주면 된다.
    
    참고 1: 생성자 1개만 있으면 @AutoWired 자동으로 붙는 효과.
    참고 2: final로 해놓으면 생성자가 필요하다고 가시적으로 알려준다. 필수 DI는 final로 하자.

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -16,24 +16,24 @@ import static org.junit.Assert.*;
 @SpringBootTest
 public class MemberRepositoryTest {
 
-    @Autowired
-    MemberRepository memberRepository;
-
-    @Test
-    @Transactional
-    @Rollback(false)
-    public void testMember() throws Exception {
-        //given
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        //when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
-
-        //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//    @Autowired
+//    MemberRepository memberRepository;
+//
+//    @Test
+//    @Transactional
+//    @Rollback(false)
+//    public void testMember() throws Exception {
+//        //given
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//
+//        //when
+//        Long savedId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(savedId);
+//
+//        //then
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
 
     }
 }
